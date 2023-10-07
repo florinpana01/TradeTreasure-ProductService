68d994c59297c71ec2fa5c1cfcf3d4a918bfee46
florinpana01 <florinpana98@gmail.com>
2023-10-07 15:36:21 +0200

    checking if the commits work

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
+  // test for commit
   imports: [],
   controllers: [AppController],
   providers: [AppService],
